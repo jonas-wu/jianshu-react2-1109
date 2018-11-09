972a8a4a4a5c6979c2ad6a61b029150baf915e04
yuzhong.wu <yuzhong.wu@ucarinc.com>
2018-11-09 19:50:27 +0800

    create

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent, Fragment } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './statics/iconfont/iconfont.css'
+
+class App extends PureComponent {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default App;
